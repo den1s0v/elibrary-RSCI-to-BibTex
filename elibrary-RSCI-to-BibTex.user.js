aa37efb34e04b2d11e3135013a32a09c54593ea7
den1s0v <hello-lumen@ya.ru>
2025-02-14 16:54:58 +0300

    Switch to www. version of the elibrary.ru site

--- elibrary-RSCI-to-BibTex.user.js
+++ elibrary-RSCI-to-BibTex.user.js
@@ -5,6 +5,7 @@
 // @description  Elibrary (Russian Science Citation Index) to BibTex article citation
 // @author       You
 // @match        https://elibrary.ru/item.asp?id=*
+// @match        https://www.elibrary.ru/item.asp?id=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
